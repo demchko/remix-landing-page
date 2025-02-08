3ead10b436e9208a1e828fbed74e28c7fb110a0a
demchkoa <demchkoartem05@gmail.com>
2025-02-08 02:42:22 +0200

    shadcn/ui init

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,5 +9,9 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <div className="text-red-500">Hello</div>;
+  return (
+    <div className="text-red-500">
+      <Button>Hello world</Button>
+    </div>
+  );
 }
